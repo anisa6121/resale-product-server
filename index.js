21c13d421a45c999ef8ae2f7604050c4fed2bd8d
anisa6121 <you@example.com>
2022-11-28 12:46:52 +0600

    get booking data

--- index.js
+++ index.js
@@ -59,6 +59,18 @@ const result = await bookingsCollection.insertOne(booking);
     });
 		
 
+        
+        // get booking data
+app.get("/bookings",  async (req, res) => {
+	const email = req.query.email;
+
+	
+
+	const query = { email: email };
+	const bookings = await bookingsCollection.find(query).toArray();
+
+	res.send(bookings);
+});
 
 
 			
